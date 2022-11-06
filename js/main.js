5d7a94ab94664c9366970ee64572d4f4d7cc971b
Hector Romero <hector_fabio.romero@uao.edu.co>
2022-11-05 21:12:44 -0500

    Ajuste de la interfaz y añadidas escenas de la introducción y la ruta 1.

--- js/main.js
+++ js/main.js
@@ -3,46 +3,46 @@ import controlEscenas from "../src/escenas.json" assert { type: "json" };
 
 const canvas = document.getElementById("canvas");
 const btnNext = document.getElementById("btnNext");
-const escenas = controlEscenas.escenas;
-let escenaActual = "Inicio";
+const toggleMenu = document.getElementById("toggle-menu");
+const closeToggleMenu = document.getElementById("close-toggle-menu");
+
+console.log(controlEscenas.introduccion);
+console.log(controlEscenas.introduccion[0].id);
+console.log(controlEscenas.introduccion[0].src);
+console.log(controlEscenas.ruta1[0].src);
+
+let escenaActual = 1;
 let decisionesTomadas = 0;
-let rutaActual = "RutaA";
+let rutaActual = "introduccion";
 
-console.log(escenas);
-console.log(escenas[0].src);
-console.log(escenas[1]);
-
-const cambiarEscena = (escenaSiguiente) => {
-	const escenaActual = controlEscenas[escenaSiguiente] || controlEscenas["Primera"];
-	console.log(escenaActual);
-	// canvas.setAttribute("src", escenaActual);
+const cambiarEscena = (idSiguiente) => {
+	// if (rutaActual === "introduccion" && idSiguiente === 8) {
+	// 	rutaActual = "ruta1";
+	// 	console.log(`Se cambió de ruta a ${rutaActual}`);
+	// 	escenaActual = 0;
+	// }
+	const escenaSiguiente = controlEscenas.introduccion[idSiguiente].src || controlEscenas.introduccion[0].src;
+	// console.log(`IdSiguiente: ${idSiguiente}, escenaSiguiente: ${escenaSiguiente}`);
+	canvas.setAttribute("src", escenaSiguiente);
+	console.log(controlEscenas.introduccion[idSiguiente].id);
 };
 
-// const elegirRuta = (opcionEscogida) => {
-// 	if (decisionesTomadas === 0) {
-// 		if (opcionEscogida === "B") {
-// 			rutaActual = "RutaE";
-// 		}
-// 	} else if (decisionesTomadas === 1) {
-// 	}
-// };
-
-const saludar = () => console.log("Hola");
-
-// Control del navbar responsive.
-const openNav = () => {
-	document.getElementById("mobile-menu").style.width = "100%";
-};
-
-const closeNav = () => {
-	document.getElementById("mobile-menu").style.width = "0%";
-};
-
-btnNext.addEventListener("click", saludar);
+btnNext.addEventListener("click", () => {
+	cambiarEscena(escenaActual);
+	escenaActual++;
+});
 
 // Evento que detecta cuando el usuario presiona la tecla 'Space'
 document.addEventListener("keypress", function onEvent(event) {
 	if (event.key === " ") {
+		// Previene que se haga scroll hacia abajo cuando el usuario presione la tecla 'Space.
+		event.preventDefault();
 		console.log("Precionaste la tecla 'Space'");
+		cambiarEscena(escenaActual);
+		escenaActual++;
 	}
 });
+
+// Control del navbar responsive.
+toggleMenu.addEventListener("click", () => (document.getElementById("mobile-menu").style.width = "100%"));
+closeToggleMenu.addEventListener("click", () => (document.getElementById("mobile-menu").style.width = "0%"));
