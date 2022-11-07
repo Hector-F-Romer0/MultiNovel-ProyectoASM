a75e545443b811785ea76a6429ac03c89f4517ca
Hector Romero <hector_fabio.romero@uao.edu.co>
2022-11-07 09:56:45 -0500

    Funcionalidad de la toma de desiciones implementada. Pendiente la optimización del código.

--- js/main.js
+++ js/main.js
@@ -3,33 +3,72 @@ import controlEscenas from "../src/escenas.json" assert { type: "json" };
 
 const canvas = document.getElementById("canvas");
 const btnNext = document.getElementById("btnNext");
+const btnA = document.getElementById("botonA");
+const btnB = document.getElementById("botonB");
 const toggleMenu = document.getElementById("toggle-menu");
 const closeToggleMenu = document.getElementById("close-toggle-menu");
 
-console.log(controlEscenas.introduccion);
-console.log(controlEscenas.introduccion[0].id);
-console.log(controlEscenas.introduccion[0].src);
-console.log(controlEscenas.ruta1[0].src);
+let decisionesTomadas = 0;
 
 let escenaActual = 1;
-let decisionesTomadas = 0;
+let esDesicion = false;
 let rutaActual = "introduccion";
 
-const cambiarEscena = (idSiguiente) => {
-	// if (rutaActual === "introduccion" && idSiguiente === 8) {
-	// 	rutaActual = "ruta1";
-	// 	console.log(`Se cambió de ruta a ${rutaActual}`);
-	// 	escenaActual = 0;
-	// }
-	const escenaSiguiente = controlEscenas.introduccion[idSiguiente].src || controlEscenas.introduccion[0].src;
-	// console.log(`IdSiguiente: ${idSiguiente}, escenaSiguiente: ${escenaSiguiente}`);
+const cambiarEscena = () => {
+	console.log(`Inicio escenaActual: ${escenaActual}`);
+	// Si el usuario se encuentra en la escena de desición, no retornará nada al entrar a este método y saldrá inmediatamente
+	if (!esDesicion === false) return;
+
+	console.log(controlEscenas[rutaActual][escenaActual].src);
+	const escenaSiguiente = controlEscenas[rutaActual][escenaActual].src || controlEscenas.introduccion[0].src;
 	canvas.setAttribute("src", escenaSiguiente);
-	console.log(controlEscenas.introduccion[idSiguiente].id);
+
+	controlDesiciones();
 };
 
-btnNext.addEventListener("click", () => {
-	cambiarEscena(escenaActual);
+const controlDesiciones = () => {
+	if (rutaActual === "introduccion" && escenaActual === 9) {
+		mostrarBotones();
+		console.log("El botón o la tecla espacio no deberían arrojar ningún error.");
+		return (esDesicion = true);
+	}
+
 	escenaActual++;
+};
+
+const opcionBoton = (botonEscogido) => {
+	if (botonEscogido === "A") {
+		// * Opciones botón A
+		if (rutaActual === "introduccion" && decisionesTomadas === 0) rutaActual = "ruta1";
+
+		ocultarBotones();
+		return;
+	}
+
+	// * Opciones botón B
+	if (rutaActual === "introduccion" && decisionesTomadas === 0) rutaActual = "ruta2";
+	ocultarBotones();
+};
+
+const mostrarBotones = () => {
+	btnA.style.display = "inline";
+	btnB.style.display = "inline";
+};
+
+const ocultarBotones = () => {
+	btnA.style.display = "none";
+	btnB.style.display = "none";
+	esDesicion = false;
+	escenaActual = 0;
+	console.log(`Ruta actual ${rutaActual}`);
+	cambiarEscena();
+};
+
+btnA.addEventListener("click", () => opcionBoton("A", rutaActual));
+btnB.addEventListener("click", () => opcionBoton("B", rutaActual));
+
+btnNext.addEventListener("click", () => {
+	cambiarEscena();
 });
 
 // Evento que detecta cuando el usuario presiona la tecla 'Space'
@@ -38,8 +77,7 @@ document.addEventListener("keypress", function onEvent(event) {
 		// Previene que se haga scroll hacia abajo cuando el usuario presione la tecla 'Space.
 		event.preventDefault();
 		console.log("Precionaste la tecla 'Space'");
-		cambiarEscena(escenaActual);
-		escenaActual++;
+		cambiarEscena();
 	}
 });
 
